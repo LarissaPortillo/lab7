55148c147a80c1bb5f28ab95884e5a9a542b7285
Glitch (hello-webpage) <none>
2020-11-04 22:19:49 +0000

    💶🏗 Checkpoint ./script.js:21940894/871

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// TODO: load the dataset 
-let airports;
-fetch("./airports.json")
-  .then(response => response.json())
-  .then(data => {
-    
-		airports= data;
-    console.log('a',airports);
+
+d3.json("./airports.json",d3.autoType)
+.then(data=>{
+  data=data;
+  console.log("a",data);
   
-    const height = 650;
-    const width= 500;
+  const height = 650;
+  const width= 500;
  
-     const svg = d3.select("body")
-    .append("svg")
-    .attr("viewBox", [0,0,width,height]) ;
+  const svg = d3.select("body")
+  .append("svg")
+  .attr("viewBox", [0,0,width,height]) ;
   
-    const size= d3.scaleLinear()
-    .domain(d3.extent(airports,d=> d.passengers))
-    .range([height,0]);
-     
-});
+  const size= d3.scaleLinear()
+  .domain(d3.extent(airports,d=> d.passengers))
+  .range([height,0]);
+  
+  
+})
