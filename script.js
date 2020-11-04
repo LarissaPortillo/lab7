8bf83c98bd99b04012b5d55b935658e4fcf74732
Glitch (hello-webpage) <none>
2020-11-04 22:55:11 +0000

    🌲🦃 Checkpoint ./script.js:21940894/249

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ d3.json("./airports.json",d3.autoType)
   .attr("viewBox", [0,0,width,height]) ;
   
   const size= d3.scaleLinear()
-  .domain(d3.extent(airports,d=> d.passengers))
+  .domain([d3.min(data,d=> d.nodes.passengers),d3.max(data,d=>d.nodes.passegers)])
   .range([height,0]);
   
-  const force = d3.forceSimulation(dataset.nodes);
+  const force = d3.forceSimulation(dataset.nodes)
+  .force("charge", d3.forceManyBody())              
+  .force("link", d3.forceLink(data.edges))              
+  .force("center", d3.forceCenter().x(width/2).y(height/2));
   
 })
