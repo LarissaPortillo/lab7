f6dc76455d48cf1bc3b7721aa98600befc5b7708
Glitch (hello-webpage) <none>
2020-11-05 05:04:07 +0000

    🌯🎭 Checkpoint ./script.js:21940894/1988

--- script.js
+++ script.js
@@ -21,9 +21,10 @@ d3.json("./airports.json",d3.autoType)
   
   const size= d3.scaleLinear()
   .domain(d3.extent( n, d=>d.passengers))
-  .range([4,10]);
+  .range([4,10]); 
   
   console.log("size",size(21663240));
+  
   
   const force = d3.forceSimulation(n)
   .force("charge", d3.forceManyBody())              
@@ -31,35 +32,9 @@ d3.json("./airports.json",d3.autoType)
   .force("x", d3.forceX())
   .force("y", d3.forceY());
 
- 
-  const links = svg.selectAll("line")               
-  .data(l)                            
-  .join("line")               
-  .style("stroke", "#ccc")               
-  .style("stroke-width", 1);
-  
-  const nodes = svg.selectAll("circle")
-  .data(n)                              
-  .join("circle")   
-  .attr("fill","plum")
-  .attr("r", d=>size( d.passengers));
-  
-  nodes.append("title")
-       .text(d=>d.name);
-
-  force.on("tick",()=>{
-    nodes.attr("cx",d=>{return d.x; })
-         .attr("cy",d=>{return d.y;}); 
-    
-    links.attr("x1", (d)=> d.source.x)         
-      .attr("y1", (d)=>  d.source.y)         
-      .attr("x2", (d)=> d.target.x)         
-      .attr("y2",(d)=>d.target.y);
-  });
-  
-  const drag= force => {
+    const drag= force => {
     function startdrag(e){
-      if(!e.active) force.alphaTarge(0.3).restart();
+      if(!e.active) force.alphaTarget(0.3).restart();
       e.subject.fx=e.subject.x;
       e.subject.fy=e.subject.y;
     }
@@ -76,7 +51,37 @@ d3.json("./airports.json",d3.autoType)
       .on("start",startdrag)
       .on("drag",dragged)
       .on("end",enddrag);
-  }
+  };
  
+ 
+  const links = svg.selectAll("line")               
+  .data(l)                            
+  .join("line")               
+  .style("stroke", "#ccc")               
+  .style("stroke-width", 1);
+  
+  const nodes = svg.selectAll("circle")
+  .data(n)                              
+  .join("circle")   
+  .attr("fill","plum")
+  .attr("r", d=>size( d.passengers))
+  .call(drag(force))
+  ;
+  
+  
+  nodes.append("title")
+       .text(d=>d.name);
+
+  force.on("tick",()=>{
+    nodes.attr("cx",d=>{return d.x; })
+         .attr("cy",d=>{return d.y;}); 
+    
+    links.attr("x1", (d)=> d.source.x)         
+      .attr("y1", (d)=>  d.source.y)         
+      .attr("x2", (d)=> d.target.x)         
+      .attr("y2",(d)=>d.target.y);
+  });
+  
+
   
 })
